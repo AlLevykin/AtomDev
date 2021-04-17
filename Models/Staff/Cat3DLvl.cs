c1cb2b528d4de662cb7f7655faa26f42712b31c5
Игорь Петров <ingvarpetrova@gmail.com>
2021-04-17 21:45:02 +0300

    added staff logic

---
+++ Models/Staff/Cat3DLvl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtomDev.Models.Staff
+{
+    public class Cat3DLvl
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/Staff/StaffMember.cs
@@ -0,0 +1,20 @@
+﻿using AtomDev.Models.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace AtomDev.Models.Staff
+{
+    public class StaffMember
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public EduOrg Workplace { get; set; }
+        public StaffCategory Category { get; set; }
+        public Cat2NdLvl Cat2NdLvl { get; set; }
+        public Cat3DLvl Cat3DLevel { get; set; }
+    }
+}
