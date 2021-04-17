096063458e6d306dce01e4a1f5c1cd9d9f48afef
AlLevykin <a.a.levykin@gmail.com>
2021-04-17 13:01:16 +0500

    typo

--- ClientApp/src/components/Students.js
+++ ClientApp/src/components/Students.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Students = () => {
     return (
         <div className="mt-5">
-        <h3 className="text-secondary">Студены</h3>
+        <h3 className="text-secondary">Студенты</h3>
         </div>        
     );
 }
